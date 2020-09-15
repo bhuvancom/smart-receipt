45b8058da95982a73dbf050d067f8ce47fcdd946
Shubhi <Hp@SHUBHI-PC>
2020-09-15 16:58:32 +0530

    initial

---
+++ src/main/java/com/shubhi/smartreceipt/SmartReceiptApplication.java
@@ -0,0 +1,13 @@
+package com.shubhi.smartreceipt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartReceiptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartReceiptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shubhi/smartreceipt/SmartReceiptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shubhi.smartreceipt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartReceiptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
